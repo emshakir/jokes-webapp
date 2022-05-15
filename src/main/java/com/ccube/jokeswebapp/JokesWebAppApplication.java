09f661c6d90b5c11aabe7770ee2112657b013181
Mohammad Shakir <65658120+Shaquib47@users.noreply.github.com>
2022-05-15 12:41:52 +0530

    added spring boot web application and also chuck norris dependencies. closes #1.closes #2.

---
+++ src/main/java/com/ccube/jokeswebapp/JokesWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.ccube.jokeswebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokesWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JokesWebAppApplication.class, args);
+	}
+
+}
